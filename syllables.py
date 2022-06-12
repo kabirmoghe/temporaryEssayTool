d370170afb7dba051fd8652ca301a00bbec8aa98
kabirmoghe <64380076+kabirmoghe@users.noreply.github.com>
2022-06-12 16:12:54 -0400

    Create syllables.py

---
+++ syllables.py
@@ -0,0 +1,67 @@
+import nltk 
+
+# You may need to run nltk.download('punkt')
+
+def syllableCounter(word):
+
+    word = word.lower()
+    
+    # Found that syllables are essentially found by looking for the transition from a consonant to a vowel
+    # This finds the number of syllables in a word by counting the number of times this occurs
+    # If a word begins with a vowel, then the number of syllables starts at 1, since there might as well be an invisible consonant before the first vowel
+
+    vowels = ["a", "e", "i", "o", "u", "y"] # Defines the vowels (including y for syllable purposes) 
+    
+    prevBool = word[0] in vowels # Whether the current (or previous) letter, first letter of the word, is a vowel or not
+
+    # If the first letter is a vowel, then begin with 1 syllable
+    if prevBool:
+        tally = 1
+    else: # If not, meaning it is a consonant, begin with 0
+        tally = 0
+        
+    # While the word has at least one letter
+    while len(word) > 0:
+
+        # Is the next letter (the first letter in the current state of "word") vowel?
+        currBool = word[0] in vowels
+
+        # Prints out the word and whether the previous and current vowels are letters
+        # print(word)
+        # print("Previous vowel?: {}".format(prevBool))
+        # print("Current Vowel: {}".format(currBool))
+
+        if currBool != prevBool and currBool:
+    
+            if word != "e": # Makes sure the last letter "e" does not add an extra syllable
+            
+                tally += 1
+            # print("increased")
+
+        prevBool = currBool    
+
+        word = word[1:]
+        # print(tally)
+        # print("--")
+        
+    return tally
+
+def stringSyllables(string):
+    words = [word for word in nltk.word_tokenize(string) if word.isalpha()]
+    syls = [syllableCounter(word) for word in words]
+    
+    total = 0
+    
+    for syl in syls:
+        total += syl
+        
+    avg = total/len(words)
+    
+    return words, syls, avg
+
+testString = "Hey, my name is Jake, and I really like computers. Do you?"
+wordList, sylList, avgSyls = stringSyllables(testString)
+
+print("Words: {}".format(wordList))
+print("Syllables: {}".format(sylList))
+print("Avg. # Syllables: {}".format(avgSyls))
